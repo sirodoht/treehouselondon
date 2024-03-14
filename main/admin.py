8b4f85c7fee8296f76bb90117c046274e863581b
Theodore Keloglou <zf@sirodoht.com>
2024-03-14 21:59:11 +0000

    add place model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+@admin.register(models.Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "url",
+        "borough",
+        "member_count",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("name", "url", "borough")
+    ordering = ["-id"]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    url = models.URLField("URL")
+    member_count = models.PositiveSmallIntegerField()
+    borough = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
