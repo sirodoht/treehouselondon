a63047391efc96ddf642084266752d9d3f16b21e
Theodore Keloglou <zf@sirodoht.com>
2024-03-16 02:28:35 +0000

    make place name unique

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Place(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     url = models.URLField("URL")
     borough = models.CharField(max_length=100)
 
